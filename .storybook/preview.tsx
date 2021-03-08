3b4f974f60cbfdef71d089251cb1f0293c3d340c
kharann <anhkhav@gmail.com>
2021-03-08 10:53:34 +0100

    fix( broken build): Fix broken build from missing depends

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,20 +1,14 @@
-import {
-  ChakraProvider,
-  Flex,
-  IconButton,
-  useColorMode,
-  useColorModeValue,
-} from "@chakra-ui/react";
-import { addParameters, StoryContext } from "@storybook/react";
-import React from "react";
-import { FaMoon, FaSun } from "react-icons/fa";
-import theme from "../src/theme";
-import Fonts from "../src/common/fonts";
+import { ChakraProvider, Flex, IconButton, useColorMode, useColorModeValue } from '@chakra-ui/react';
+import { addParameters, StoryContext } from '@storybook/react';
+import React from 'react';
+import { FaMoon, FaSun } from 'react-icons/fa';
+import { OnlineTheme } from '../src/system/theme';
+import Fonts from '../src/common/fonts';
 
 const ColorModeToggleBar = () => {
   const { toggleColorMode } = useColorMode();
   const SwitchIcon = useColorModeValue(FaMoon, FaSun);
-  const nextMode = useColorModeValue("dark", "light");
+  const nextMode = useColorModeValue('dark', 'light');
 
   return (
     <Flex justify="flex-end" mb={4}>
@@ -36,7 +30,7 @@ const withChakra = (StoryFn: Function, context: StoryContext) => {
   return (
     <>
       <Fonts />
-      <ChakraProvider theme={theme}>
+      <ChakraProvider theme={OnlineTheme}>
         <ColorModeToggleBar />
         <StoryFn />
       </ChakraProvider>
@@ -47,5 +41,5 @@ const withChakra = (StoryFn: Function, context: StoryContext) => {
 export const decorators = [withChakra];
 
 addParameters({
-  viewMode: "canvas",
+  viewMode: 'canvas',
 });
